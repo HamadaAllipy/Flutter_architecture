b72c28ccf19b45d6ee4d86d8df8e97251faaeb77
Hamada Mohammed Seif <hamada.devlop@gmail.com>
2022-06-25 19:16:39 +0200

    Creating base view model

---
+++ lib/presentation/base/base_viewmodel.dart
@@ -0,0 +1,9 @@
+abstract class BaseViewModel {}
+
+abstract class InputBaseViewModel extends BaseViewModel {
+  void start();
+
+  void dispose();
+}
+
+abstract class OutBaseViewModel extends BaseViewModel {}
