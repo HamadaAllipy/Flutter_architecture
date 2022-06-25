bf6cd77373e2c25fc90fe6ea817b5333b67d4f3b
Hamada Mohammed Seif <hamada.devlop@gmail.com>
2022-06-25 15:18:41 +0200

    circle indicator icons added

--- lib/presentation/res/assets_manager.dart
+++ lib/presentation/res/assets_manager.dart
@@ -1,5 +1,6 @@
 
 const String _imagesPath = 'assets/images';
+const String _iconPath = 'assets/icons';
 class ImagesManager{
   ImagesManager._();
 
@@ -8,4 +9,10 @@ class ImagesManager{
   static const String imageOnBoarding2 = '$_imagesPath/image_onBoarding_2.svg';
   static const String imageOnBoarding3 = '$_imagesPath/image_onBoarding_3.svg';
   static const String imageOnBoarding4 = '$_imagesPath/image_onBoarding_4.svg';
+}
+class IconsManager{
+  IconsManager._();
+
+  static const String circleHollow = '$_iconPath/circle_hollow.svg';
+  static const String circleSolid = '$_iconPath/circle_solid.svg';
 }
